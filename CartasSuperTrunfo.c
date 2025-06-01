653669ae03735451f1671272475519d25dfc2916
EduToledo504 <Edu_drt@outlook.com>
2025-06-01 04:41:29 +0000

    Modelo das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,3 +6,7 @@ struct Carta {
     float pib;
     int pontosTuristicos;
  };
+ 
+ int main () {
+ struct Carta carta1 = {"Cidade Z", 500000, 120.5, 8};
+ struct Carta carta2 = {"Cidade E", 750000, 98.2, 12};
