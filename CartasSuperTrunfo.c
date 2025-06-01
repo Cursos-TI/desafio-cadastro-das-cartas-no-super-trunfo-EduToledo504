1ac189d25db5d1ee8ad1b945d03ca3385d43d90f
EduToledo504 <Edu_drt@outlook.com>
2025-06-01 04:42:14 +0000

    Abrindo o arquivo das cartas para escrita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,3 +10,9 @@ struct Carta {
  int main () {
  struct Carta carta1 = {"Cidade Z", 500000, 120.5, 8};
  struct Carta carta2 = {"Cidade E", 750000, 98.2, 12};
+
+ File *arquivo = fopen("cartas.txt", "w");
+if (arquivo == NULL) {
+printf("Erro ao abrir o arquivo!\n");
+return 1;
+}
