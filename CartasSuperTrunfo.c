0a9d1a2162398becd4ea94665813659489985359
EduToledo504 <Edu_drt@outlook.com>
2025-06-01 04:45:41 +0000

    Fechando o arquivo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -27,3 +27,9 @@ fprintf(arquivo, "---- Carta 2 ----\n");
 fprintf(arquivo, "Nome: %s\n", carta2.nome);
 fprintf(arquivo, "População: %d\n", carta2.populacao);
 fprintf(arquivo, "Pontos Turistico: %d\n", carta 2.pontosTuristicos);
+fclose(arquivo);
+
+printf("Cartas salvas com sucesso no arquivo 'cartas.txt.\n");
+
+return 0;
+}
