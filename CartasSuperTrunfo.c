d1981f20ca02263eb0cf623c92d49a1687587eca
EduToledo504 <Edu_drt@outlook.com>
2025-06-01 04:44:21 +0000

    salvando as cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 struct Carta {
-    char nome{[50]
+    char nome[50]
     int populacao;
     float pib;
     int pontosTuristicos;
@@ -16,3 +16,9 @@ if (arquivo == NULL) {
 printf("Erro ao abrir o arquivo!\n");
 return 1;
 }
+
+fprintf(arquivo, "----- Carta 1 ----\n");
+fprintf(arquivo, "Nome: %s\n", carta1.nome);
+fprintf(arquivo, "População:%d\n", carta1.populacao);
+fprintf(arquivo, "PIB: %.2f bilhões\n", carta1.pib;
+fprintf(arquivo, "Pontos Turísticos: %d\n\n", carta1.pontosTuristicos);
